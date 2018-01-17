9627baa31ca3baa6309deca752398d5ba37d1282
William Mau <wmau@bu.edu>
2018-01-17 17:11:56 -0500

    For working with calcium traces

---
+++ calcium_traces.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 14:11:14 2018
+
+@author: William Mau
+"""
+
+from os import path
+from pickle import load
+from pandas import read_csv
+
+master_directory = 'U:\Fear conditioning project_Mosaic2\SessionDirectories'
+file = path.join(master_directory,'SessionDirectories.pkl')
+session_list = load(open(file,'rb'))
+
+def load_traces(session_index):
+    trace_file = path.join(session_list[session_index]["Location"],'Traces.csv')
+
+    with open(trace_file,'r') as csv_file:
+        accepted = read_csv(csv_file,nrows=1).T
+        
+    with open(trace_file,'r') as csv_file:
+        traces = read_csv(csv_file,skiprows=2).T
+           
+    return traces,accepted
