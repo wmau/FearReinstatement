6b64c1431f5609d2b4acb5c779f3881819d28a7f
William Mau <wmau@bu.edu>
2018-01-17 17:40:55 -0500

    Improvements to calcium_traces

--- calcium_traces.py
+++ calcium_traces.py
@@ -8,6 +8,7 @@ Created on Wed Jan 17 14:11:14 2018
 from os import path
 from pickle import load
 from pandas import read_csv
+from numpy import delete
 
 master_directory = 'U:\Fear conditioning project_Mosaic2\SessionDirectories'
 file = path.join(master_directory,'SessionDirectories.pkl')
@@ -19,11 +20,20 @@ def load_traces(session_index):
 
     #Get accepted/rejected list. 
     with open(trace_file,'r') as csv_file:
-        accepted = read_csv(csv_file,nrows=1).T
+        accepted = read_csv(csv_file,nrows=1).T  
         
+    #Delete the first row, which is like a header. 
+    accepted = accepted.iloc[1:]
+    
     #For reasons I don't understand yet, read_csv modifies your position on 
     #the CSV file so we need to reload the file. Now, actually get the traces. 
     with open(trace_file,'r') as csv_file:
-        traces = read_csv(csv_file,skiprows=2).T
+        traces = read_csv(csv_file,skiprows=2).T.as_matrix()
+        
+    #Extract the time vector. 
+    t = traces[0,:] 
+    
+    #Delete time vector from traces. 
+    traces = delete(traces,(0),axis=0)
            
-    return traces,accepted
+    return traces,accepted,t
