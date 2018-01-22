a41191638f62afdf39542994eb8f33beba05686e
William Mau <wmau@bu.edu>
2018-01-22 18:09:36 -0500

    Module for looking at basic statistics for cells

---
+++ cell_stats.py
@@ -0,0 +1,12 @@
+from os import path
+from glob import glob
+from session_directory import load_session_list
+
+session_list = load_session_list()
+
+def get_number_of_ICs(session_index):
+    directory = path.join(session_list[session_index]["Location"],'ROIs')
+    ROIs = glob(path.join(directory,'ROIs_C*.tiff'))
+
+    n_ICs = len(ROIs)
+    return n_ICs
