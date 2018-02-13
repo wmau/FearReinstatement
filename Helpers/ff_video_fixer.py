966381ce16fd9d28087a9030e0718f225e6f990b
William Mau <wmau@bu.edu>
2018-02-13 16:58:20 -0500

    Comments and bugfix

--- Helpers/ff_video_fixer.py
+++ Helpers/ff_video_fixer.py
@@ -30,6 +30,9 @@ class FFObj:
         self.movie = color.rgb2gray(self.movie)
 
     def get_ff_files(self):
+        """
+        Find FreezeFrame files by searching the directory.
+        """
         ff_dir = path.join(session_list[self.session_index]["Location"], "FreezeFrame")
         csv_location = glob(path.join(ff_dir, '*.csv'))
         avi_location = glob(path.join(ff_dir, '*.avi'))
@@ -44,6 +47,9 @@ class FFObj:
         return csv_location, avi_location
 
     def get_timestamps(self):
+        """
+        Open the CSV file imported from FreezeFrame.
+        """
         with open(self.csv_location, 'r') as csv_file:
             data = read_csv(csv_file)
 
@@ -51,6 +57,10 @@ class FFObj:
         return t
 
     def scroll_through_frames(self):
+        """
+        Scroll through all the frames of the movie. Currently the default starting point
+        is the middle.
+        """
         titles = ["Frame " + str(n) for n in range(self.n_frames)]
 
         f = ScrollPlot(plot_funcs.display_frame,
@@ -60,6 +70,11 @@ class FFObj:
         return f
 
     def select_region(self, frame_num):
+        """
+        Selects a region from a frame of your choice.
+        :param frame_num:
+        :return:
+        """
         # Convert frame into image object then pass through the FrameSelector class.
         frame = Image.fromarray(self.movie[frame_num])
         frameObj = FrameSelector(frame)
@@ -74,32 +89,41 @@ class FFObj:
     def stitch_baseline_frame(self, from_frame, to_frame):
         """
         Crop a region from from_frame and paste it to to_frame.
-        :param from_frame:
-        :param to_frame:
-        :return:
+        :param
+            from_frame: frame number you want to paste onto to_frame.
+            to_frame: frame number you want from_Frame to paste to.
         """
+        # Get the selected area and the rectangle associated with it.
         chunk, region = self.select_region(from_frame)
         paste_onto_me = Image.fromarray(self.movie[to_frame])
 
+        # Paste and also convert the image to grayscale.
         paste_onto_me.paste(chunk, region)
         self.baseline_frame = color.rgb2gray(np.array(paste_onto_me))
 
     def get_baseline_frame(self):
+        """
+        Build the baseline frame.
+        """
+        # Scroll through the frames.
         f = self.scroll_through_frames()
         fig_num = f.fig.number
 
+        # While the figure is still open, keep going.
         while plt.fignum_exists(fig_num):
             plt.waitforbuttonpress(0)
 
+        # When you hit ESC, prompt for frame numbers.
         else:
             from_frame = int(input("From what frame do you want to cut out a region?"))
             to_frame = int(input("On what frame do you want to paste that region?"))
 
+        # Stitch the partial frames together.
         self.stitch_baseline_frame(from_frame, to_frame)
 
     def auto_detect_mouse(self, smooth_sigma=6, threshold=0.15):
         MouseDetectorObj = MouseDetector(self.baseline_frame, self.movie, smooth_sigma)
-        MouseDetectorObj.detect_mouse(threshold)
+        self.position = MouseDetectorObj.detect_mouse(threshold)
 
 
 class FrameSelector:
@@ -214,11 +238,21 @@ class MouseDetector:
         self.d_movie = self.make_difference_movie()
 
     def delta_baseline(self, frame):
+        """
+        Find the difference of a frame from the baseline, then smooth.
+        :param
+            frame: frame that you want to subtract from baseline.
+        """
+        # Subtract and smooth.
         delta = gfilt(self.baseline - frame, self.sigma)
 
         return delta
 
     def make_difference_movie(self):
+        """
+        Take the difference between every frame and the baseline.
+        """
+        # Preallocate then for each frame, subtract it from the baseline.
         d_movie = np.zeros(self.movie.shape)
         for i, frame in enumerate(self.movie):
             d_movie[i] = self.delta_baseline(frame)
@@ -226,11 +260,20 @@ class MouseDetector:
         return d_movie
 
     def threshold_movie(self, d_movie, threshold):
+        """
+        Threshold the difference movie.
+        :param
+            d_movie: stacked frames of the difference between frames and baseline.
+            threshold: scalar [0,1].
+        """
         thresh_movie = [cv2.inRange(frame, threshold, 1) for frame in d_movie]
 
         return thresh_movie
 
     def build_blob_detector(self):
+        """
+        Set the parameters for the blob detector here.
+        """
         params = cv2.SimpleBlobDetector_Params()
         params.filterByColor = False
         params.filterByConvexity = False
@@ -243,16 +286,18 @@ class MouseDetector:
         return detector
 
     def detect_mouse(self, threshold):
-        d_movie = self.make_difference_movie()
-        thresh_movie = self.threshold_movie(d_movie, threshold)
-        detector = self.build_blob_detector()
+        d_movie = self.make_difference_movie()                      # Make difference movie.
+        thresh_movie = self.threshold_movie(d_movie, threshold)     # Make thresholded movie.
+        detector = self.build_blob_detector()                       # Build blob detector.
 
+        # Detect blobs on each frame.
         position = np.zeros([self.n_frames, 2])
         for i, frame in enumerate(thresh_movie):
             blobs = detector.detect(frame)
 
             blob_sizes = [this_blob.size for this_blob in blobs]
 
+            # Find the biggest blob. This is usually where the mouse is most likely.
             try:
                 biggest = max(blob_sizes)
                 biggest_blob = blob_sizes.index(biggest)
@@ -261,6 +306,7 @@ class MouseDetector:
             except:
                 position[i] = [0, 0]
 
+        # Plot frame and position of mouse.
         titles = ["Frame " + str(n) for n in range(self.n_frames)]
 
         f = ScrollPlot(plot_funcs.display_frame_and_position,
@@ -271,8 +317,8 @@ class MouseDetector:
 
 # FF = FFObj(0)
 # FF.scroll_through_frames()
-FF = FFObj(0)
+# FF = FFObj(0)
 # FF.inquire_user_for_baseline_inputs()
-FF.auto_detect_mouse()
+# FF.auto_detect_mouse()
 
 pass
