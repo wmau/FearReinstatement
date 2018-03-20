9024b476ef368159f65b65002e641566347e7da9
William Mau <wmau@bu.edu>
2018-03-20 09:01:37 -0400

    Added filter for good neurons

--- Helpers/helper_functions.py
+++ Helpers/helper_functions.py
@@ -4,3 +4,8 @@ def find_closest(array, value):
     idx = (np.abs(array - value)).argmin()
 
     return array[idx],idx
+
+def filter_good_neurons(accepted):
+    neurons = [cell_number for cell_number, good in enumerate(accepted) if good]
+
+    return neurons
