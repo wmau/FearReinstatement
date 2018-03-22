22523490d7fc515fd86cec3af10502e012389918
William Mau <wmau@bu.edu>
2018-03-22 15:49:06 -0400

    Rewrote ismember.m

--- Helpers/helper_functions.py
+++ Helpers/helper_functions.py
@@ -9,3 +9,14 @@ def filter_good_neurons(accepted):
     neurons = [cell_number for cell_number, good in enumerate(accepted) if good]
 
     return neurons
+
+def ismember(A,B):
+    """
+    Returns an array containing logical True where data in A is found in B. Also returns
+    indices in B for every value of A.
+    """
+
+    A_in_B = np.in1d(A,B)
+    B_idx = np.nonzero(A_in_B)[0]
+
+    return A_in_B, B_idx
