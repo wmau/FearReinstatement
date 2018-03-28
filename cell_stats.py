db5525c6ac5959365ee764fdbf400d9ebe27a458
William Mau <wmau@bu.edu>
2018-03-28 12:36:47 -0400

    Bugfix

--- cell_stats.py
+++ cell_stats.py
@@ -6,7 +6,7 @@ session_list = load_session_list()
 
 def get_number_of_ICs(session_index):
     directory = path.join(session_list[session_index]["Location"],'ROIs')
-    ROIs = glob(path.join(directory,'ROIs_C*.tiff'))
+    ROIs = glob(path.join(directory,'ROIs_C*.*'))
 
     n_ICs = len(ROIs)
     return n_ICs
