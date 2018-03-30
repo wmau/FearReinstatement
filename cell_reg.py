b10c4ff8ee1e8807fac0a2b8f69e1243b6c4871e
William Mau <wmau@bu.edu>
2018-03-30 15:32:02 -0400

    Find the session index in match_matrix

--- cell_reg.py
+++ cell_reg.py
@@ -1,10 +1,12 @@
 from os import path, rename
-from session_directory import load_session_list, find_mouse_directory
+from session_directory import load_session_list, find_mouse_directory, \
+    find_mouse_sessions
 import calcium_traces as ca_traces
 import glob
 import h5py
 import numpy as np
 import pickle
+from helper_functions import find_dict_index
 
 session_list = load_session_list()
 
@@ -47,21 +49,66 @@ def exclude_bad_cells_in_this_mouse(mouse):
             rename_rejected_ROIs(session_index)
 
 def load_cellreg_results(mouse):
-        mouse_directory = find_mouse_directory(mouse)
-        cellreg_directory = path.join(mouse_directory,'CellRegResults')
-        cellreg_file = path.join(cellreg_directory,'CellRegResults.pkl')
-        with open(cellreg_file, 'rb') as file:
-            match_map, centroids, footprints = pickle.load(file)
+    """
+    After having already running CellRegObj, load the saved pkl file.
+    :param mouse:
+    :return:
+    """
+    # Find the directory and navigate to the pkl file.
+    mouse_directory = find_mouse_directory(mouse)
+    cellreg_directory = path.join(mouse_directory,'CellRegResults')
+    cellreg_file = path.join(cellreg_directory,'CellRegResults.pkl')
 
-        return match_map, centroids, footprints
+    # Open pkl file.
+    with open(cellreg_file, 'rb') as file:
+        match_map, centroids, footprints = pickle.load(file)
+
+    return match_map, centroids, footprints
+
+def find_match_map_index(session_indices):
+    """
+    Find the index reference for the match_map matrix.
+    :param session_indices:
+    :return:
+    """
+    idx = []
+    for session_index in session_indices:
+        # Get all session data.
+        mouse = session_list[session_index]["Animal"]
+        date = session_list[session_index]["Date"]
+        session = session_list[session_index]["Session"]
+
+        # Find sessions from this mosue.
+        _, sessions = find_mouse_sessions(mouse)
+
+        # Find all sessions with the specified date and session number.
+        idx_date = find_dict_index(sessions, "Date", date)
+        idx_session = find_dict_index(sessions, "Session", session)
+
+        # Get session that matches the specified session_index.
+        matched_session = list(set(idx_date) & set(idx_session))
+
+        # Make sure there's only one.
+        assert len(matched_session) is 1, "Multiple sessions with these fields!"
+
+        idx.append(matched_session[0])
+
+    return idx
+
 
 class CellRegObj:
     def __init__(self,mouse):
         self.mouse = mouse
-        self.data,self.file,self.mouse_directory = self.read_cellreg_output()
+        self.data,self.file,self.mouse_directory = \
+            self.read_cellreg_output()
         self.compile_cellreg_data()
 
     def read_cellreg_output(self):
+        """
+        Reads the .mat file.
+        :return:
+        """
+        # Get directory.
         mouse_directory = find_mouse_directory(self.mouse)
 
         # Get the .mat file name.
@@ -77,21 +124,27 @@ class CellRegObj:
         return data, file, mouse_directory
 
     def process_registration_map(self):
-        match_map = self.data['cell_to_index_map'].value.T
+        # Get the cell_to_index_map. Reading the file transposes the
+        # matrix. Transpose it back.
+        cell_to_index_map = self.data['cell_to_index_map'].value.T
+        match_map = cell_to_index_map - 1
 
-        return match_map
+        return match_map.astype(int)
 
     def process_spatial_footprints(self):
+        # Get the spatial footprints after translations.
         footprints_reference = self.data['spatial_footprints_corrected'].value[0]
 
         footprints = []
         for idx in footprints_reference:
+            # Float 32 takes less memory.
             session_footprints = np.float32(np.transpose(self.file[idx].value,(2,0,1)))
             footprints.append(session_footprints)
 
         return footprints
 
     def process_centroids(self):
+        # Also get centroid positions after translations.
         centroids_reference = self.data['centroid_locations_corrected'].value[0]
 
         centroids = []
@@ -102,6 +155,8 @@ class CellRegObj:
         return centroids
 
     def compile_cellreg_data(self):
+        # Gets registration information. So far, this consists of the
+        # cell to index map, centroids, and spatial footprints.
         match_map = self.process_registration_map()
         centroids = self.process_centroids()
         footprints = self.process_spatial_footprints()
@@ -109,8 +164,9 @@ class CellRegObj:
         filename = path.join(self.cellreg_results_directory,'CellRegResults.pkl')
 
         with open(filename,'wb') as output:
-            pickle.dump([match_map, centroids, footprints],output,protocol=4)
+            pickle.dump([match_map, centroids, footprints],
+                        output,protocol=4)
 
 
 if __name__ == '__main__':
-    load_cellreg_results('Kerberos')
+    find_match_map_index(11)
