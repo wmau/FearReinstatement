ef68bd5e70fc0974b50b7fb32a7fc5ffe6f6d200
William Mau <wmau@bu.edu>
2018-05-01 11:33:39 -0400

    Plotting ordered cells

--- sequences/seqNMF_data.py
+++ sequences/seqNMF_data.py
@@ -6,27 +6,75 @@ import numpy as np
 session_list = load_session_list()
 
 class seqNMF:
-    def __init__(self, session_index, var_names):
+    def __init__(self, session_index, var_names=('H','W','XC','thres'),
+                 mat_file='seqNMF_results.mat'):
         self.session_index = session_index
         self.mouse = session_list[session_index]["Animal"]
         self.directory = session_list[session_index]["Location"]
-        self.filename = path.join(self.directory,'seqNMF_results.mat')
+        self.filename = path.join(self.directory, mat_file)
 
         self.data = self.load_data(var_names)
+        self.count_sequences()
+        self.sequence_cells = self.get_sequential_cells()
+        self.order = self.get_sequence_order()
 
     def load_data(self, var_names):
         data = sio.loadmat(self.filename, variable_names=var_names)
 
         return data
 
-    def get_number_of_sequences(self):
+    def count_sequences(self):
         try:
             self.n_sequences = np.sum(self.data['H'].any(axis=1))
         except:
             data = sio.loadmat(self.filename, variable_names='H')
             self.n_sequences = np.sum(data['H'].any(axis=1))
 
+    def get_sequential_cells(self):
+        try:
+            XC = self.data['XC']
+            thres = self.data['thres']
+        except:
+            raise ValueError('Variables XC and/or thres do not exist')
+
+        # Get statistically significant cells for each sequence.
+        significant_cells = []
+        n_time_bins = XC.shape[1]
+        for sequence, thresholds in zip(range(self.n_sequences), thres):
+            # Cross correlation of each cell.
+            if self.n_sequences == 1:
+                xcorr = XC
+            else:
+                xcorr = XC[:,:,sequence]
+            thresholds = np.tile(thresholds,[n_time_bins,1]).T
+
+            # Time bins where cross correlation exceeds null.
+            significant_bins = np.where((xcorr > thresholds).any(axis=1))[0]
+            significant_cells.append(significant_bins)
+
+        return significant_cells
+
+    def get_sequence_order(self):
+        try:
+            W = self.data['W']
+        except:
+            raise ValueError('Variable W does not exist')
+
+        # Get statistically significant cells.
+        significant_cells = self.get_sequential_cells()
+        order = []
+
+        # Get their rank based on their power in that factor.
+        for factor_power, cells in \
+                zip(np.reshape(W, (W.shape[1],W.shape[0],-1)),
+                    significant_cells):
+            peaks = np.argmax(factor_power[cells],
+                              axis=1).astype(int)
+            order.append(np.argsort(peaks).astype(int))
+
+        return order
+
 
 if __name__ == '__main__':
-    S = seqNMF(0, ['XC','H','W'])
-    pass
+    S = seqNMF(5)
+    S.get_sequential_cells()
--- sequences/sequence_plotting.py
+++ sequences/sequence_plotting.py
@@ -7,45 +7,43 @@ from scipy.stats import zscore
 import data_preprocessing as d_pp
 import ff_video_fixer as ff
 from matplotlib import gridspec
+import calcium_events as ca_events
+from cell_reg import load_cellreg_results, find_match_map_index, \
+    find_cell_in_map
 
 session_list = load_session_list()
 
-def plot_sequences(session_index, zero_time=True):
-    # Load data.
-    seqNMF_results = seqNMF(session_index, ['H','W','XC','thres'])
+def plot_ordered_cells(session_index, cells, order=None, dtype='event'):
+    if order is None:
+        order = range(len(cells))
+
     session = ff.load_session(session_index)
-    traces, t = ca_traces.load_traces(session_index)
+    n_cells = len(cells)
+
+    if type(cells) is list:
+        cells = np.asarray(cells)
+
+    if dtype == 'trace':
+        data, t = ca_traces.load_traces(session_index)
+        data = zscore(data, axis=1)
+    elif dtype == 'event':
+        data,t  = ca_events.load_events(session_index)
+        data[data > 0] = 1
+    else:
+        raise ValueError('Wrong dtype value.')
 
     # Trim all the time series.
-    traces = d_pp.trim_session(traces, session.mouse_in_cage)
+    traces = d_pp.trim_session(data, session.mouse_in_cage)
     t = d_pp.trim_session(t, session.mouse_in_cage)
+    t -= min(t)
     v = d_pp.trim_session(session.imaging_v, session.mouse_in_cage)
     freezing = d_pp.trim_session(session.imaging_freezing,
                                  session.mouse_in_cage)
 
-    # Can either zero the time so start of the session is when mouse
-    # enters chamber or don't zero the time in which case teh start of
-    # the session is when recording begins.
-    if zero_time:
-        t -= min(t)
-
-    # Normalize.
-    traces = zscore(traces, axis=1)
-
-    # Dump results.
-    XC, H, W, thres = seqNMF_results.data['XC'], \
-                      seqNMF_results.data['H'], \
-                      seqNMF_results.data['W'], \
-                      seqNMF_results.data['thres']
-
-    # Get significantly contributing cells.
-    significant_cells = np.where((XC > thres.T).any(axis=1))[0]
-    n_significant_cells = len(significant_cells)
-
-    # Determine the order of the cells.
-    W = np.squeeze(W[significant_cells,1,:])
-    peaks = np.argmax(W, axis=1).astype(int)
-    order = np.argsort(peaks).astype(int)
+    if dtype == 'event':
+        events = []
+        for cell in traces:
+            events.append(np.where(cell)[0]/20)
 
     # Plot.
     gs = gridspec.GridSpec(4, 1)
@@ -55,22 +53,121 @@ def plot_sequences(session_index, zero_time=True):
     velocity_plot = fig.add_subplot(gs[0,:])
     plt.plot(t, v, 'k')
     velocity_plot.set_ylabel('Velocity',color='k')
-
-    # Freezing plot.
-    freezing_plot = velocity_plot.twinx()
-    plt.plot(t, freezing, 'r')
-    freezing_plot.set_ylabel('Freezing',color='r')
+    velocity_plot.fill_between(t, 0, v.max(), freezing,
+                               facecolor='r')
     plt.setp(velocity_plot.get_xticklabels(), visible=False)
 
     # Raster plot.
-    fig.add_subplot(gs[1:,0], sharex=velocity_plot)
-    plt.imshow(traces[significant_cells[order]],
-               extent=[t[0], t[-1], n_significant_cells, 0])
+    event_plot = fig.add_subplot(gs[1:,0], sharex=velocity_plot)
+
+    if dtype == 'trace':
+        plt.imshow(traces[cells[order]],
+                   extent=[t[0], t[-1], n_cells, 0])
+    elif dtype == 'event':
+        plt.eventplot([events[x] for
+                       x in cells[order]])
+
+        event_plot.fill_between(t, -0.5, n_cells+0.5, freezing,
+                                facecolor='r', alpha=0.4)
+        event_plot.invert_yaxis()
+
     plt.axis('tight')
     plt.xlabel('Time (s)')
     plt.ylabel('Cell #')
 
-    pass
+def plot_ordered_cells_across_days(session_1, session_2, cells,
+                                   order=None, dtype='event'):
+    if order is None:
+        order = range(len(cells))
+
+    # Get mouse name and ensure that you're matching correct animals.
+    mouse = session_list[session_1]["Animal"]
+    assert mouse == session_list[session_2]["Animal"], \
+        "Animal names do not match."
+
+    # Load cell matching map.
+    cell_map = load_cellreg_results(mouse)
+    map_idx = find_match_map_index([session_1, session_2])
+
+    global_cell_idx = find_cell_in_map(cell_map, map_idx[0], cells)
+    local_s2_cells = cell_map[global_cell_idx, map_idx[1]]
+    sorted_s2_cells = local_s2_cells[order]
+
+    sorted_s2_cells = sorted_s2_cells[sorted_s2_cells > -1]
+    n_cells = len(sorted_s2_cells)
+
+    plot_ordered_cells(session_2, sorted_s2_cells, range(n_cells),
+                       dtype=dtype)
+
+
+def plot_sequences(session_index, dtype='event',
+                   mat_file='seqNMF_results.mat'):
+    # Load data.
+    seqNMF_results = seqNMF(session_index, mat_file=mat_file)
+
+    # Get significantly contributing cells.
+    significant_cells = seqNMF_results.get_sequential_cells()
+
+    # Determine the order of the cells.
+    sequence_orders = seqNMF_results.get_sequence_order()
+
+    # Plot.
+    for cells, order in zip(significant_cells, sequence_orders):
+        plot_ordered_cells(session_index, cells, order, dtype=dtype)
+
+def plot_sequences_across_days(session_1, session_2, dtype='event',
+                               mat_file='seqNMF_results.mat'):
+    # Get mouse name and ensure that you're matching correct animals.
+    mouse = session_list[session_1]["Animal"]
+    assert mouse == session_list[session_2]["Animal"], \
+        "Animal names do not match."
+
+    # Get sequence information from session 1 and traces from session 2.
+    s1_sequence = seqNMF(session_1, mat_file=mat_file)
+
+    if dtype == 'trace':
+        data, t = ca_traces.load_traces(session_2)
+        data = zscore(data, axis=1)
+    elif dtype == 'event':
+        data,t  = ca_events.load_events(session_2)
+        data[data > 0] = 1
+    else:
+        raise ValueError('Wrong dtype value.')
+
+    session = ff.load_session(session_2)
+
+    # Trim and process trace information and freezing/velocity.
+    traces = d_pp.trim_session(data, session.mouse_in_cage)
+
+    if dtype == 'event':
+        events = []
+        for cell in traces:
+            events.append(np.where(cell)[0]/20)
+
+    # Get statistically significant cells in sequence and their rank.
+    significant_cells = s1_sequence.get_sequential_cells()
+    sequence_orders = s1_sequence.get_sequence_order()
+
+    # Load cell matching map.
+    cell_map = load_cellreg_results(mouse)
+    map_idx = find_match_map_index([session_1, session_2])
+
+    # For each sequence from seqNMF...
+    for cells, order in zip(significant_cells, sequence_orders):
+        # Get cell index and sort them.
+        global_cell_idx = find_cell_in_map(cell_map, map_idx[0], cells)
+        local_s2_cells = cell_map[global_cell_idx, map_idx[1]]
+        sorted_s2_cells = local_s2_cells[order]
+
+        # Delete cells that didn't match.
+        sorted_s2_cells = sorted_s2_cells[sorted_s2_cells > -1]
+        n_cells = len(sorted_s2_cells)
+
+        plot_ordered_cells(session_2, sorted_s2_cells, range(n_cells),
+                           dtype=dtype)
+
 
 if __name__ == '__main__':
-    plot_sequences(4)
+    from single_cell_analyses.footshock import ShockSequence
+    FC = ShockSequence(0)
+    plot_ordered_cells_across_days(0, 1, FC.shock_modulated_cells, FC.order)
