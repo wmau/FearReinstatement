9d1f0ba47a8270d3632d0a29cff80ab053043bc1
William Mau <wmau@bu.edu>
2018-05-08 11:34:04 -0400

    Correlation matrices across days

--- population_analyses/correlation_changes.py
+++ population_analyses/correlation_changes.py
@@ -5,56 +5,155 @@ import ff_video_fixer as ff
 import numpy as np
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
 from sklearn.cluster import k_means
+from scipy.stats import zscore
+import seaborn as sns
+import cell_reg
+import helper_functions as helper
+from scipy.spatial import distance
 
-def time_slice_correlation(session_index, bin_length, neurons):
+session_list = load_session_list()
 
-    session = ff.load_session(session_index)
+def do_sliced_correlation(session_index, bin_length, neurons):
+    traces, t, bins = d_pp.trim_and_bin(session_index, neurons=neurons,
+                                        samples_per_bin=bin_length * 20)
 
-    traces, t = ca_traces.load_traces(session_index)
-    if neurons is None:
-        neurons = range(len(traces))
-
-    t = d_pp.trim_session(t, session.mouse_in_cage)
-    traces = d_pp.trim_session(traces, session.mouse_in_cage)
     n_neurons = len(neurons)
-
-    samples_per_bin = bin_length * 20
-    bins = d_pp.make_bins(t, samples_per_bin)
     n_samples = len(bins) + 1
 
     corr_matrices = np.zeros((n_samples, n_neurons, n_neurons))
-    binned_traces = d_pp.bin_time_series(traces, bins)
-    for i, sample in enumerate(binned_traces):
+    for i, sample in enumerate(traces):
         corr_matrices[i] = np.corrcoef(sample)
 
-    return corr_matrices
+    return corr_matrices, t
 
-def cluster_corr_matrices(corr_matrices, time_slice=0, n_clusters=5):
-    _, cluster_idx, _ = k_means(corr_matrices[time_slice], n_clusters)
+def cluster_corr_matrices(corr_matrices, cluster_here, n_clusters, t):
+    for idx, time_slice in enumerate(t):
+        if cluster_here in time_slice:
+            break
+
+    _, cluster_idx, _ = k_means(corr_matrices[idx], n_clusters)
     order = np.argsort(cluster_idx)
 
     return cluster_idx, order
 
-def plot_corr_matrices(session_idx, bin_length=60, neurons=None,
-                       cluster=True, time_slice=0, n_clusters=5):
-    corr_matrices = time_slice_correlation(session_idx,
-                                           bin_length=bin_length,
-                                           neurons=neurons)
+def plot_corr_matrices_over_time(session_idx, bin_length=10,
+                                 neurons=None, cluster_here=0,
+                                 n_clusters=5):
 
-    if cluster:
-        _, order = cluster_corr_matrices(corr_matrices, time_slice=time_slice,
-                                        n_clusters=n_clusters)
+    # Do correlations on time slices.
+    corr_matrices,t  = do_sliced_correlation(session_idx,
+                                             bin_length=bin_length,
+                                             neurons=neurons)
 
-        for i in range(11):
-            plt.subplot(4,3,i+1)
-            plt.imshow(corr_matrices[i,order,:][order])
-
+    if n_clusters > 0:
+        _, order = cluster_corr_matrices(corr_matrices, cluster_here,
+                                         n_clusters, t)
     else:
-        for i in range(11):
-            plt.subplot(4,3,i+1)
-            plt.imshow(corr_matrices[i])
+        order = range(corr_matrices.shape[1])
 
-    pass
+    plot_corr_matrices(corr_matrices, order)
+
+def plot_corr_matrices(corr_matrices, order, clusters=None):
+    n_time_slices = len(corr_matrices)
+    n_cols = int(np.floor(n_time_slices**(.5)))
+    n_rows = int(np.ceil(n_time_slices / n_cols))
+    gs = gridspec.GridSpec(n_rows, n_cols, wspace = 0.025, hspace=0.05)
+    fig = plt.figure(figsize=(n_cols, n_rows))
+
+    # Plot correlation matrices.
+    for i, matrix in enumerate(corr_matrices):
+        ax = fig.add_subplot(gs[i])
+        ax.set_yticks([])
+        ax.set_xticks([])
+        plt.imshow(matrix[:,order][order],cmap='bwr',vmin=-1,vmax=1)
+
+        # Plot clusters.
+        if clusters is not None:
+            clusters.sort()
+            cluster_boundaries = np.where(np.diff(clusters))[0]
+            for boundary in cluster_boundaries:
+                plt.axvline(x=boundary, c='k', lw=1)
+                plt.axhline(y=boundary, c='k', lw=1)
+
+
+def compute_matrices_across_days(session_1, session_2, bin_length=10,
+                                 neurons=None, n_clusters=5,
+                                 cluster_here=0):
+    # Load cell map.
+    mouse = session_list[session_1]["Animal"]
+    assert mouse == session_list[session_2]["Animal"], "Mice don't match."
+    match_map = cell_reg.load_cellreg_results(mouse)
+    map_idx = cell_reg.find_match_map_index((session_1, session_2))
+
+    # If neurons are specified, narrow down the list.
+    if neurons is not None:
+        found, idx = helper.ismember(neurons, match_map[:, map_idx[0]])
+        match_map = match_map[idx[found]]
+
+    # Only take cells that persisted across the sessions.
+    match_map = cell_reg.trim_match_map(match_map, map_idx)
+    neurons_ref = match_map[:, 0]
+    neurons_test = match_map[:, 1]
+
+    # Do correlations on time slices.
+    corr_matrices_1, t = do_sliced_correlation(session_1, bin_length,
+                                               neurons_ref)
+    corr_matrices_2, _ = do_sliced_correlation(session_2, bin_length,
+                                               neurons_test)
+
+    # Cluster correlation matrix at specified time slice.
+    if n_clusters > 0:
+        clusters, order = cluster_corr_matrices(corr_matrices_1,
+                                                cluster_here,
+                                                n_clusters, t)
+    else:
+        order = range(len(neurons_ref))
+        clusters = None
+
+    return corr_matrices_1, corr_matrices_2, order, clusters
+
+def plot_matrices_across_days(session_1, session_2, bin_length=10,
+                              neurons=None, n_clusters=5, cluster_here=0):
+
+    corr_matrices_1, corr_matrices_2, order, clusters = \
+        compute_matrices_across_days(session_1, session_2,
+                                     bin_length=bin_length,
+                                     neurons=neurons,
+                                     n_clusters=n_clusters,
+                                     cluster_here=cluster_here)
+
+    plot_corr_matrices(corr_matrices_1, order, clusters=clusters)
+    plot_corr_matrices(corr_matrices_2, order, clusters=clusters)
+
+
+def cosine_distance_between_matrices(session_1, session_2, bin_length=10,
+                                     neurons=None, ref_time=0):
+    corr_matrices_1, corr_matrices_2, order, clusters = \
+        compute_matrices_across_days(session_1, session_2,
+                                     bin_length=bin_length,
+                                     neurons=neurons, n_clusters=0)
+
+    _, t, _ = d_pp.trim_and_bin(session_1, samples_per_bin=bin_length*20)
+    for idx, time_slice in enumerate(t):
+        if ref_time in time_slice:
+            break
+
+    reference_matrix = corr_matrices_1[idx].flatten()
+
+    distances = np.zeros(len(corr_matrices_2))
+    for i, matrix in enumerate(corr_matrices_2):
+        distances[i] = distance.cosine(reference_matrix, matrix.flatten())
+
+    plt.figure()
+    plt.plot(distances)
+
+    return distances
+
 if __name__ == '__main__':
-    plot_corr_matrices(0, n_clusters=2)
+    # cell_map = cell_reg.load_cellreg_results('Fenrir')
+    # cell_map = cell_reg.trim_match_map(cell_map, [10, 11, 12, 14])
+    # neurons = cell_map[:, 0]
+    # cosine_distance_between_matrices(10,11,time_slice=-2,neurons=neurons)
+    plot_matrices_across_days(10,11,cluster_here=698, n_clusters=10)
