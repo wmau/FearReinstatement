34d6900626100df03167c126a8c1438c5730bb2c
William Mau <wmau@bu.edu>
2018-05-23 17:19:46 -0400

    Plot cosine similarity with color as freezing

--- population_analyses/correlation_changes.py
+++ population_analyses/correlation_changes.py
@@ -6,12 +6,15 @@ import numpy as np
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
+from matplotlib.collections import LineCollection
 from sklearn.cluster import k_means
 from scipy.stats import zscore
 import seaborn as sns
 import cell_reg
 import helper_functions as helper
 from scipy.spatial import distance
+from session_directory import find_mouse_sessions
+from behavior.freezing import compute_percent_freezing
 
 session_list = load_session_list()
 
@@ -132,26 +135,97 @@ def plot_matrices_across_days(session_1, session_2, bin_length=10,
 
 def cosine_distance_between_matrices(session_1, session_2, bin_length=10,
                                      neurons=None, ref_time=0):
+    """
+    Perform pairwise correlations between cells within days then
+    linearize these matrices and get their cosine distance across
+    days from a specified time bin in session_1.
+
+    Parameters
+    ---
+    session_1 and session_2: sessions to analyze.
+    bin_length: scalar, size of time bins in seconds.
+    neurons: array-like, neurons to include in analysis from session_1
+    ref_time: scalar, time in seconds for the reference time bin from
+    session_1.
+
+    Returns
+    ---
+    distances: (N,) array containing cosine distances from each time
+    bin in session_2 to the reference in session_1.
+
+    """
+    # Get correlation matrices.
     corr_matrices_1, corr_matrices_2, order, clusters = \
         compute_matrices_across_days(session_1, session_2,
                                      bin_length=bin_length,
                                      neurons=neurons, n_clusters=0)
 
+    # Need the time bin containing the scalar ref_time. Slice up the
+    # time vector the same way we did to get the correlation matrices.
+    # Then find the time slice that ref_time is in.
     _, t, _ = d_pp.trim_and_bin(session_1, samples_per_bin=bin_length*20)
     for idx, time_slice in enumerate(t):
         if ref_time in time_slice:
             break
 
-    reference_matrix = corr_matrices_1[idx].flatten()
+    # Flatten the correlation matrix at ref_time. Only take the upper
+    # triangle.
+    reference_matrix = corr_matrices_1[idx]
+    reference_vector = reference_matrix[np.triu_indices_from(reference_matrix,
+                                                             k=1)]
 
+    # Get cosine distances.
     distances = np.zeros(len(corr_matrices_2))
     for i, matrix in enumerate(corr_matrices_2):
-        distances[i] = distance.cosine(reference_matrix, matrix.flatten())
+        vector = matrix[np.triu_indices_from(matrix, k=1)]
+        distances[i] = distance.cosine(reference_vector, vector)
 
-    plt.figure()
-    plt.plot(distances)
+    similarity = -(distances-1)
+    p_freezing, _ = compute_percent_freezing(session_2,
+                                             bin_length=bin_length)
 
-    return distances
+    segment = np.asarray([np.asarray(range(len(similarity))),
+                          similarity]).T.reshape(-1, 1, 2)
+    segment = np.concatenate([segment[:-1], segment[1:]], axis=1)
+
+    # Plot.
+    f, ax = plt.subplots(1, 1)
+
+    norm = plt.Normalize(0, 1)
+    line_to_draw = LineCollection(segment, cmap='OrRd', norm=norm)
+    line_to_draw.set_array(p_freezing)
+
+    line = ax.add_collection(line_to_draw)
+    plt.ylabel('Cosine similarity')
+    plt.xlabel('Time bin')
+    ax.set_xlim(0, len(similarity))
+    ax.set_ylim(similarity.min(), similarity.max())
+    f.colorbar(line, ax=ax)
+
+    return similarity
+
+
+def do_cosine_distance_analysis(mouse, bin_length=1, neurons=None,
+                                ref_time=938):
+    sessions, _ = find_mouse_sessions(mouse)
+
+    s1 = sessions[0]
+    s2 = sessions[[1,2,4]]
+
+    d = []
+    for i, session in enumerate(s2):
+        d.append(cosine_distance_between_matrices(s1, session,
+                                                  bin_length=bin_length,
+                                                  neurons=neurons,
+                                                  ref_time=ref_time))
+
+        if i == 0:
+            ylims = plt.gca().get_ylim()
+
+        if i > 0:
+            plt.gca().set_ylim(ylims)
+
+    return d
 
 def pairwise_correlate_traces(session_index, neurons=None):
     """
@@ -200,24 +274,28 @@ def cosine_distance_between_days(session_1, session_2, neurons=None):
 
     return d
 
+
+
 if __name__ == '__main__':
     # cell_map = cell_reg.load_cellreg_results('Fenrir')
     # cell_map = cell_reg.trim_match_map(cell_map, [10, 11, 12, 14])
     # neurons = cell_map[:, 0]
-    # cosine_distance_between_matrices(10,11,time_slice=-2,neurons=neurons)
+    # cosine_distance_between_matrices(10,11,ref_time=938,neurons=neurons)
     # plot_matrices_across_days(10,11,cluster_here=698, n_clusters=10)
 
-    s1 = [0, 5, 10, 15]
-    s2 = [[1, 2, 4], [6, 7, 9], [11, 12, 14], [16, 17, 19]]
-    all_sessions = [[0, 1, 2, 4], [5, 6, 7, 9], [10, 11, 12, 14],
-                    [15, 16, 17, 19]]
+    # s1 = [0, 5, 10, 15]
+    # s2 = [[1, 2, 4], [6, 7, 9], [11, 12, 14], [16, 17, 19]]
+    # all_sessions = [[0, 1, 2, 4], [5, 6, 7, 9], [10, 11, 12, 14],
+    #                 [15, 16, 17, 19]]
+    #
+    # d = np.zeros((4,3))
+    # for i, fc in enumerate(s1):
+    #     match_map = cell_reg.load_cellreg_results(session_list[fc]['Animal'])
+    #     trimmed = cell_reg.trim_match_map(match_map,all_sessions[i])
+    #     neurons = trimmed[:,0]
+    #     for j, s in enumerate(s2[i]):
+    #         d[i,j] = cosine_distance_between_days(fc, s, neurons=neurons)
+    #
+    # pass
 
-    d = np.zeros((4,3))
-    for i, fc in enumerate(s1):
-        match_map = cell_reg.load_cellreg_results(session_list[fc]['Animal'])
-        trimmed = cell_reg.trim_match_map(match_map,all_sessions[i])
-        neurons = trimmed[:,0]
-        for j, s in enumerate(s2[i]):
-            d[i,j] = cosine_distance_between_days(fc, s, neurons=neurons)
-
-    pass
+    do_cosine_distance_analysis('Kerberos')
